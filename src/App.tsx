a957f68e3a63fadad671ec9860504baf8ec92566
lukrai <lukrai007@gmail.com>
2019-04-28 11:31:39 +0300

    Added weather icons to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './main.scss';
 
 const App: React.FC = () => {
     return (
@@ -8,7 +7,19 @@ const App: React.FC = () => {
                 <a href="#">WeatherApp</a>
             </header>
             <main>
-                <h1>Not a Header</h1>
+                <div className="search-box">
+                    <form className="search-form" /*onSubmit={this.handleSubmit}*/>
+                        <input
+                            type="text"
+                        />
+                        <button type="submit">
+                            <svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
+                                <path d="M0 0h24v24H0z" fill="none"/>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
                 <div className="weather-grid">
                     <div className="weather-city-title">
                         <p>Kaunas</p>
@@ -19,7 +30,7 @@ const App: React.FC = () => {
                     <div className="weather-info">
                         <p>Kaunas</p>
                     </div>
-                    <div className="day-grid">
+                    <div className="weather-day-grid">
                         <div className="weather-day">
                             <p>1</p>
                         </div>
